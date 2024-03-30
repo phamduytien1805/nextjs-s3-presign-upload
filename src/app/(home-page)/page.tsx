24603d7d8eb8543f9b43703070c56776a46e0c57
PHAM DUY TIEN <duytien@MacBook-Pro-cua-PHAM.local>
2024-03-30 22:56:21 +0700

    init source

---
+++ src/app/(home-page)/page.tsx
@@ -0,0 +1,17 @@
+import { Box, SimpleGrid } from "@chakra-ui/react";
+
+export default async function HomePage() {
+
+  return (
+    <SimpleGrid columns={1} spacing={10} height={'100vh'}>
+    <Box bg='tomato' height='80px'></Box>
+    <Box bg='tomato' height='80px'></Box>
+    <Box bg='tomato' height='80px'></Box>
+    <Box bg='tomato' height='80px'></Box>
+    <Box bg='tomato' height='80px'></Box>
+    <Box bg='tomato' height='80px'></Box>
+    <Box bg='tomato' height='80px'></Box>
+
+  </SimpleGrid>
+  )
+}
